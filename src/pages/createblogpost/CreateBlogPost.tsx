d9a993377acf83d1bee358e8dccc39bae5ff01a4
Jay <mantine08@gmail.com>
2023-02-06 13:07:26 -0800

    Cleaned up formatting

--- src/pages/createblogpost/CreateBlogPost.tsx
+++ src/pages/createblogpost/CreateBlogPost.tsx
@@ -1,76 +1,146 @@
-import {
-  Button,
-  FormGroup,
-  FormLabel,
-  Grid,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material'
+import { Box, Button, FormLabel, Stack, TextField } from '@mui/material'
 import { Container } from '@mui/system'
+import React from 'react'
+
+const pictureUrls = [
+  'https://via.placeholder.com/150',
+  'https://via.placeholder.com/150',
+  'https://via.placeholder.com/150',
+  'https://via.placeholder.com/150',
+  'https://via.placeholder.com/150',
+  'https://via.placeholder.com/150',
+]
 
 export const CreateBlogPost = () => {
+  const [pictureIndexStart, setPictureIndexStart] = React.useState(0)
+  const [selectedPictureIndex, setSelectedPictureIndex] = React.useState(0)
+  const [title, setTitle] = React.useState('')
+  const [body, setBody] = React.useState('')
+
+  React.useEffect(() => {
+    console.log(selectedPictureIndex)
+  }, [selectedPictureIndex])
   return (
     <>
       <Container>
-        <Stack
-          direction='column'
-          justifyContent='flex-start'
-          alignItems='flex-start'
-          spacing={8}
+        <form
+          style={{
+            alignSelf: 'stretch',
+            justifyItems: 'space-between',
+            flex: 1,
+          }}
         >
-          <form style={{ alignSelf: 'stretch' }}>
-            <Stack direction='column' spacing={7}>
-              <FormLabel>Pick an image</FormLabel>
-              <Stack justifyContent={'space-between'} direction='row'>
-                <Button>{'<'}</Button>
-                <Stack direction='row' spacing={5}>
-                  <Button>dsa</Button>
-                  <Button>dsa</Button>
-                  <Button>dsa</Button>
-                  <Button>dsa</Button>
-                </Stack>
-                <Button>{'>'}</Button>
+          <Stack
+            direction='column'
+            justifyContent='flex-start'
+            alignItems='flex-start'
+            spacing={5}
+          >
+            <FormLabel>Pick an image</FormLabel>
+            <Stack
+              justifyContent={'space-between'}
+              alignSelf={'stretch'}
+              direction='row'
+            >
+              <Button
+                onClick={() => {
+                  if (pictureIndexStart - 4 < 0) {
+                    setPictureIndexStart(
+                      Math.ceil(pictureUrls.length / 4) * 4 - 4,
+                    )
+                  } else {
+                    setPictureIndexStart(
+                      (pictureIndexStart - 4) % pictureUrls.length,
+                    )
+                  }
+                }}
+              >
+                {'<'}
+              </Button>
+              <Stack direction='row' spacing={5}>
+                {Array(4)
+                  .fill(null)
+                  .map((value, index) => {
+                    return (
+                      <Button
+                        style={{
+                          backgroundColor:
+                            selectedPictureIndex === pictureIndexStart + index
+                              ? 'black'
+                              : 'transparent',
+                        }}
+                        disabled={!pictureUrls[pictureIndexStart + index]}
+                        key={index}
+                        onClick={() => {
+                          setSelectedPictureIndex(pictureIndexStart + index)
+                        }}
+                      >
+                        <Box
+                          sx={{
+                            width: 150,
+                            height: 150,
+                            backgroundImage: `url(${
+                              pictureUrls[pictureIndexStart + index]
+                            })`,
+                          }}
+                        />
+                      </Button>
+                    )
+                  })}
               </Stack>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'flex-start',
-                  justifyContent: 'space-between',
-                  alignSelf: 'stretch',
+              <Button
+                onClick={() => {
+                  if (pictureIndexStart + 4 >= pictureUrls.length) {
+                    setPictureIndexStart(0)
+                  } else {
+                    setPictureIndexStart(
+                      (pictureIndexStart + 4) % pictureUrls.length,
+                    )
+                  }
                 }}
               >
-                <FormLabel style={{ flex: 0.05 }}>Title</FormLabel>
-                <TextField
-                  style={{ flex: 0.95 }}
-                  variant='outlined'
-                  placeholder='60 words or less'
-                />
-              </div>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'flex-start',
-                  justifyContent: 'space-between',
-                  alignSelf: 'stretch',
-                }}
-              >
-                <FormLabel style={{ flex: 0.05 }}>Body</FormLabel>
-                <TextField
-                  style={{ flex: 0.95 }}
-                  multiline
-                  rows={7}
-                  variant='outlined'
-                  placeholder='250 words or less'
-                />
-              </div>
+                {'>'}
+              </Button>
             </Stack>
-          </form>
-
-          <Button variant='contained'>CREATE</Button>
-        </Stack>
+            <Stack
+              direction={'row'}
+              alignItems={'flex-start'}
+              justifyContent={'space-between'}
+              alignSelf={'stretch'}
+            >
+              <FormLabel style={{ flex: 0.05 }}>Title</FormLabel>
+              <TextField
+                style={{ flex: 0.95 }}
+                variant='outlined'
+                placeholder='60 words or less'
+                onChange={(event) => {
+                  setTitle(event.target.value)
+                }}
+              />
+            </Stack>
+            <Stack
+              direction={'row'}
+              alignItems={'flex-start'}
+              justifyContent={'space-between'}
+              alignSelf={'stretch'}
+            >
+              <FormLabel style={{ flex: 0.05 }}>Body</FormLabel>
+              <TextField
+                style={{ flex: 0.95 }}
+                multiline
+                rows={7}
+                variant='outlined'
+                placeholder='250 words or less'
+                onChange={(event) => {
+                  setBody(event.target.value)
+                }}
+              />
+            </Stack>
+          </Stack>
+          <Button type='submit' variant='contained' style={{ marginTop: 34 }}>
+            CREATE
+          </Button>
+        </form>
       </Container>
     </>
   )
