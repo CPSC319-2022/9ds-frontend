9358ad536489660559c673328fbfe704fefb4658
patrick <pgousse@student.ubc.ca>
2023-02-07 19:15:40 -0800

    fixed merge issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button,Grid, IconButton, Menu, MenuItem, Toolbar, Typography} from "@mui/material";
+import { AppBar, Box, Button,Grid, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { useState } from "react";
@@ -17,30 +17,30 @@ export const Header = () => {
 
   const textTheme = {textTransform: 'capitalize', fontFamily:'roboto', fontSize: 16, px:1};
 
-  return <Box sx={{py:2, px:2}}>
-            <AppBar position="static" color='transparent' sx={{ borderRadius:2, border:2, py:1}}>
+  return <Box sx={{py:16, px:16, color:'black'}}>
+            <AppBar position="static" color='transparent' sx={{color: 'black', borderRadius:2, border:2, py:1}}>
               <Toolbar>
                   <Grid container   direction="row"
                         justifyContent="space-between"
                         alignItems="center">
                   <Box
                       component="img"
-                      sx={{ ml:1 }}
+                      sx={{ ml:24, borderRadius:3 }}
                       alt="Logo"
                       src={require('../assets/logo.png')}
                     />                  
                       <Grid item  >
-                        <Box sx={{ gap:8, color: 'black', flexGrow: 1, display:{ xs: 'none', md: 'flex'} }}>
+                        <Box sx={{ gap:96, flexGrow: 1, display:{ xs: 'none', md: 'flex'} }}>
                           <Button
                             onClick={() => navigate('/')}
-                            sx={{ color: 'black', display: 'block' }}
+                            sx={{ color: 'black.main'}}
                           >
                             <Typography sx={textTheme}>
                               Home
                             </Typography>
                           </Button>
                           <IconButton
-                            sx={{ color:'black'}}
+                            sx={{ color:'black.main'}}
                             onClick={handleClick}
                           >
                             <Typography sx={textTheme}>
@@ -60,7 +60,7 @@ export const Header = () => {
                         </Menu>
                           <Button
                             onClick={() => navigate('/about')}
-                            sx={{color: 'black', display: 'block' }}
+                            sx={{color: 'black.main', display: 'block' }}
                           >
                             <Typography sx={textTheme}>
                               About Us
@@ -70,20 +70,20 @@ export const Header = () => {
                       </Grid>
 
                       <Grid item>
-                        <Box sx={{ gap: 2, flexGrow: 1, display:  'flex' }}>
+                        <Box sx={{ gap: 16, flexGrow: 1, display:  'flex'}}>
                           <Button
                             onClick={() => navigate('/login')}
-                            sx={{ border: 1,color: 'black', display: 'block' }}
+                            sx={{ border: 1,color: 'black.main', display: 'block' }}
                           >
-                            <Typography sx={textTheme}>
+                            <Typography sx={{ fontSize:14}}>
                               Login
                             </Typography>
                             </Button>
                           <Button
                             onClick={() => navigate('/signup')}
-                            sx={{ backgroundColor: 'black', border: 1,color: 'white', display: 'block', mr:1}}
+                            sx={{ backgroundColor: 'black.main', border: 1,color: 'white.main', display: 'block', mr:24}}
                           >
-                            <Typography sx={textTheme}>
+                            <Typography sx={{ fontSize:14}}>
                               Sign Up
                             </Typography>
                           </Button>
