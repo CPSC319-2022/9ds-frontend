cbb74659cba4ace3fdd3cf267c7233eacc5826eb
ubcyukiny <yukiny@student.ubc.ca>
2023-02-10 10:44:08 -0800

    installed @mui/icons-material in package.json

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,12 @@
 import ThemeProvider from '@mui/material/styles/ThemeProvider'
 import Home from '../pages/home/Home'
-import Login from '../pages/login/Login'
 import React from 'react'
 import { theme } from '../theme/Theme'
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-        {/*// change to Home later*/}
-      <Login />
+    <Home/>
     </ThemeProvider>
   )
 }
