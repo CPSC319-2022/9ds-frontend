337629fd1a47a961fd8486417242577f7debd880
StuartLiv <h.stuart.livingstone@gmail.com>
2023-02-22 19:47:39 -0800

    added project configs

--- src/index.tsx
+++ src/index.tsx
@@ -18,44 +18,61 @@ interface gcpConfigInterface {
   apiKey: string
 }
 
-let gcpConfig: gcpConfigInterface = {projectId: "", apiKey: ""}
+let firebaseConfig;
 
 switch(process.env.REACT_APP_ENV) {
   
   case "DEV":
-    gcpConfig = {
+    firebaseConfig = {
+      apiKey: "AIzaSyCHfU9yLnEnLeKKokIQ9sUGI8Cr9mLYXgE",
+      authDomain: "ds-blog-dev.firebaseapp.com",
       projectId: "ds-blog-dev",
-      apiKey: "AIzaSyCGGe5ZDxWeJITHmfr3Xn2he1iFWBdhe3I"
+      storageBucket: "ds-blog-dev.appspot.com",
+      messagingSenderId: "545518666660",
+      appId: "1:545518666660:web:85badd3f2024480bc07731",
+      measurementId: "G-BB9M09710P"
     };
     break;
   
   case "QA":
-    gcpConfig = {
+    firebaseConfig = {
+      apiKey: "AIzaSyCG-Lvza6UMVVSW_gZ_oHQFMQa3sssDgdk",
+      authDomain: "ds-blog-qa.firebaseapp.com",
       projectId: "ds-blog-qa",
-      apiKey: "AIzaSyDKFaX7kzx02Ca9qjjU5_NbSJVLdpOFcjw"
-    }
+      storageBucket: "ds-blog-qa.appspot.com",
+      messagingSenderId: "631919673252",
+      appId: "1:631919673252:web:3b5bd30368a67c23ba4b31",
+      measurementId: "G-59FZFJ0980"
+    };
     break;
 
   case "PROD":
-    gcpConfig = {
+    firebaseConfig = {
+      apiKey: "AIzaSyCcgzkgm7DJkNLTrBk-vDQgeTZw7bTwdn8",
+      authDomain: "ds-blog-376905.firebaseapp.com",
       projectId: "ds-blog-376905",
-      apiKey: "AIzaSyA3GTkgZ2andw7-szFQ2Crnp8-pNbx9Av4"
+      storageBucket: "ds-blog-376905.appspot.com",
+      messagingSenderId: "1046661353798",
+      appId: "1:1046661353798:web:e6771cdb184606a1861547",
+      measurementId: "G-V1G11MYNRB"
     }
     break;
   
   default:
-    gcpConfig = {
+    firebaseConfig = {
       projectId: "dev-emulator",
       apiKey: "dev-emulator-placeholder-key"
   }
 }
 
-const app = initializeApp(gcpConfig)
+const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 
 if (!process.env.REACT_APP_ENV) {
   connectFirestoreEmulator(db, 'localhost', 8080);
+  connectAuthEmulator(auth, 'https://localhost:9099', { disableWarnings: true })
 }
 
-export const auth = getAuth(app);
-connectAuthEmulator(auth, 'https://localhost:9099', { disableWarnings: true })
+
+
