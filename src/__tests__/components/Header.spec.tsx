2f1b67d5534fa4d5d5b3536876350da2c1b42ea4
brian <r7c2b@ugrad.cs.ubc.ca>
2023-03-01 17:23:17 -0800

    PWC-73: Figma Alignment with Header

--- src/__tests__/components/Header.spec.tsx
+++ src/__tests__/components/Header.spec.tsx
@@ -20,22 +20,22 @@ describe('Header Styling', () => {
     expect(logo).toHaveAttribute('width', '100px')
     expect(logo).toHaveAttribute('height', '50px')
 
-    const homeHeading = screen.getByText('Home')
+    const homeHeading = screen.getByText('HOME')
     expect(homeHeading).toBeInTheDocument()
     expect(homeHeading).toHaveClass(
-      'MuiTypography-root MuiTypography-subheading css-37jdci-MuiTypography-root',
+        'MuiTypography-root MuiTypography-subheading css-1nrr9hi-MuiTypography-root',
     )
 
-    const blogHeading = screen.getByText('Blog')
+    const blogHeading = screen.getByText('BLOG')
     expect(blogHeading).toBeInTheDocument()
     expect(blogHeading).toHaveClass(
-      'MuiTypography-root MuiTypography-subheading css-37jdci-MuiTypography-root',
+      'MuiTypography-root MuiTypography-subheading css-1nrr9hi-MuiTypography-root',
     )
 
-    const aboutUsHeading = screen.getByText('About Us')
+    const aboutUsHeading = screen.getByText('ABOUT US')
     expect(aboutUsHeading).toBeInTheDocument()
     expect(aboutUsHeading).toHaveClass(
-      'MuiTypography-root MuiTypography-subheading css-37jdci-MuiTypography-root',
+      'MuiTypography-root MuiTypography-subheading css-1nrr9hi-MuiTypography-root',
     )
 
     const [loginButton, signUpButton] = screen.getAllByRole('button')
