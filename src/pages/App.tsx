5fc9bb2815137b677e3b3eab4f798675ba447757
StuartLiv <h.stuart.livingstone@gmail.com>
2023-03-02 17:29:47 -0800

    cleaned local tree

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,6 @@ import { theme } from '../theme/Theme'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import { CreateArticle } from './createarticle/CreateArticle'
 import Login from './login/Login'
-import { IndividualBlogPost } from './blogpost/IndividualBlogPost'
 export const App = () => {
   return (
     <Router>
@@ -14,7 +13,6 @@ export const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/create' element={<CreateArticle />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/blog' element={<IndividualBlogPost />} />
           {/* 
         Left here as template for next addition of routes
         <Route path="/sign-in" element={<Signin />}>
