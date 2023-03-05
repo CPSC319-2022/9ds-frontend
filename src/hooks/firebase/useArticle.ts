910a3329e0107456abe1ff571fd7605de3c201c3
Jay <mantine08@gmail.com>
2023-03-05 12:35:55 -0800

    Refactored article form

--- src/hooks/firebase/useArticle.ts
+++ src/hooks/firebase/useArticle.ts
@@ -14,13 +14,15 @@ import {
   QuerySnapshot,
   FirestoreErrorCode,
   QueryDocumentSnapshot,
-  FirestoreError, getDoc, getDocs, startAfter
+  FirestoreError,
+  getDoc,
+  getDocs,
+  startAfter,
 } from 'firebase/firestore'
-import {auth, db} from '../../index'
+import { auth, db } from '../../index'
 import { useState, useEffect } from 'react'
-import {getUser, UserData} from './useUser'
+import { getUser, UserData } from './useUser'
 import { comment } from './useComment'
-
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
@@ -34,21 +36,23 @@ export interface ArticlePreview {
   articleId: string
 }
 
-export const articlePreviewTranslator = (docs: QuerySnapshot<DocumentData>): ArticlePreview[] => {
-    const articlesData: ArticlePreview[] = []
-    docs.forEach((doc) => {
-        articlesData.push({
-            title: doc.data().title,
-            content: doc.data().content,
-            header_image: doc.data().header_image,
-            author_image: doc.data().author_image,
-            author_username: doc.data().author_username,
-            publish_time: doc.data().publish_time,
-            articleId: doc.id
-        })
+export const articlePreviewTranslator = (
+  docs: QuerySnapshot<DocumentData>,
+): ArticlePreview[] => {
+  const articlesData: ArticlePreview[] = []
+  docs.forEach((doc) => {
+    articlesData.push({
+      title: doc.data().title,
+      content: doc.data().content,
+      header_image: doc.data().header_image,
+      author_image: doc.data().author_image,
+      author_username: doc.data().author_username,
+      publish_time: doc.data().publish_time,
+      articleId: doc.id,
     })
+  })
 
-    return articlesData
+  return articlesData
 }
 
 export interface article {
@@ -74,31 +78,33 @@ export const useArticleRecents = (n: number) => {
     orderBy('publish_time'),
   )
 
-  let lastArticle: QueryDocumentSnapshot<DocumentData>;
-  let endOfCollection = false;
+  let lastArticle: QueryDocumentSnapshot<DocumentData>
+  let endOfCollection = false
 
   useEffect(() => {
     getDocs(query(q, limit(n)))
-        .then((docs: QuerySnapshot<DocumentData>) => {
-            setLoading(false);
-            setArticles(articlePreviewTranslator(docs))
-            lastArticle = docs.docs[docs.docs.length - 1]
-            endOfCollection = docs.docs.length < n
-        }).catch((err: FirestoreError) => {
+      .then((docs: QuerySnapshot<DocumentData>) => {
+        setLoading(false)
+        setArticles(articlePreviewTranslator(docs))
+        lastArticle = docs.docs[docs.docs.length - 1]
+        endOfCollection = docs.docs.length < n
+      })
+      .catch((err: FirestoreError) => {
         setError(err.code)
       })
   }, [])
 
   const getNext = (n: number) => {
-      setLoadingNext(true)
-      getDocs(query(q, startAfter(lastArticle), limit(n)))
-          .then((docs: QuerySnapshot<DocumentData>) => {
-              setLoadingNext(false)
-              setArticles(articles.concat(articlePreviewTranslator(docs)))
-              lastArticle = docs.docs[docs.docs.length - 1]
-              endOfCollection = docs.docs.length < n
-      }).catch((err: FirestoreError) => {
-          setError(err.code)
+    setLoadingNext(true)
+    getDocs(query(q, startAfter(lastArticle), limit(n)))
+      .then((docs: QuerySnapshot<DocumentData>) => {
+        setLoadingNext(false)
+        setArticles(articles.concat(articlePreviewTranslator(docs)))
+        lastArticle = docs.docs[docs.docs.length - 1]
+        endOfCollection = docs.docs.length < n
+      })
+      .catch((err: FirestoreError) => {
+        setError(err.code)
       })
   }
 
@@ -112,7 +118,7 @@ export const useArticleRead = (articleID: string) => {
 
   useEffect(() => {
     getDoc(doc(db, 'article', articleID))
-        .then((doc) => {
+      .then((doc) => {
         const data = doc.data()
         if (data === undefined) {
           setError('not-found')
@@ -120,10 +126,10 @@ export const useArticleRead = (articleID: string) => {
           setLoading(false)
           setArticle(data as article)
         }
-      }).catch((err: FirestoreError) => {
+      })
+      .catch((err: FirestoreError) => {
         setError(err.code)
-      }
-    )
+      })
   }, [articleID])
 
   return { error, loading, article }
@@ -135,46 +141,47 @@ export const useArticleComments = (articleID: string, n: number) => {
   const [comments, setComments] = useState<comment[]>([])
   const [loadingNext, setLoadingNext] = useState(true)
 
-  const q = query(collection(db, `article/${articleID}/comments`));
+  const q = query(collection(db, `article/${articleID}/comments`))
 
-  let lastComment: QueryDocumentSnapshot<DocumentData>;
-  let endOfCollection = false;
+  let lastComment: QueryDocumentSnapshot<DocumentData>
+  let endOfCollection = false
 
   useEffect(() => {
     getDocs(query(q, limit(n)))
-        .then((docs: QuerySnapshot<DocumentData>) => {
-            const commentsData: DocumentData[] = []
-            docs.forEach((doc) => {
-                commentsData.push(doc.data)
-            })
-            setLoading(false)
-            setComments(commentsData as comment[])
-            lastComment = docs.docs[docs.docs.length - 1]
-            endOfCollection = docs.docs.length < n
-      }).catch((err: FirestoreError) => {
-            setError(err.code)
+      .then((docs: QuerySnapshot<DocumentData>) => {
+        const commentsData: DocumentData[] = []
+        docs.forEach((doc) => {
+          commentsData.push(doc.data)
+        })
+        setLoading(false)
+        setComments(commentsData as comment[])
+        lastComment = docs.docs[docs.docs.length - 1]
+        endOfCollection = docs.docs.length < n
       })
-
+      .catch((err: FirestoreError) => {
+        setError(err.code)
+      })
   }, [articleID])
 
-    const getNext = (n: number) => {
-        setLoadingNext(true)
-        getDocs(query(q, startAfter(lastComment), limit(n)))
-            .then((docs: QuerySnapshot<DocumentData>) => {
-                const commentsData: DocumentData[] = []
-                docs.forEach((doc) => {
-                    commentsData.push(doc.data)
-                })
-                setLoadingNext(false)
-                setComments(comments.concat(commentsData as comment[]))
-                lastComment = docs.docs[docs.docs.length - 1]
-                endOfCollection = docs.docs.length < n
-            }).catch((err: FirestoreError) => {
-            setError(err.code)
+  const getNext = (n: number) => {
+    setLoadingNext(true)
+    getDocs(query(q, startAfter(lastComment), limit(n)))
+      .then((docs: QuerySnapshot<DocumentData>) => {
+        const commentsData: DocumentData[] = []
+        docs.forEach((doc) => {
+          commentsData.push(doc.data)
         })
-    }
+        setLoadingNext(false)
+        setComments(comments.concat(commentsData as comment[]))
+        lastComment = docs.docs[docs.docs.length - 1]
+        endOfCollection = docs.docs.length < n
+      })
+      .catch((err: FirestoreError) => {
+        setError(err.code)
+      })
+  }
 
-  return {getNext, error, loading, loadingNext, comments, endOfCollection }
+  return { getNext, error, loading, loadingNext, comments, endOfCollection }
 }
 
 export const useArticleCreate = () => {
@@ -183,37 +190,39 @@ export const useArticleCreate = () => {
   const [articleId, setArticleId] = useState<string>()
 
   const createArticle = (
-      title: string,
-      content: string,
-      header_image: string,
-      published: boolean) => {
+    title: string,
+    content: string,
+    header_image: string,
+    published: boolean,
+  ) => {
+    getUser(auth.currentUser === null ? null : auth.currentUser.uid)
+      .then((user: UserData) =>
+        addDoc(collection(db, 'article'), {
+          author_uid: user.uid,
+          author_image: user.profile_image,
+          author_username: user.username,
+          content: content,
+          edit_time: serverTimestamp(),
+          header_image: header_image,
+          published: published,
+          publish_time: published ? serverTimestamp() : null,
+          title: title,
+        }).then(
+          (doc) => {
+            setLoading(false)
+            setArticleId(doc.id)
+          },
+          (err) => {
+            setError(err.code)
+          },
+        ),
+      )
+      .catch((err) => {
+        setError('unauthenticated')
+      })
+  }
 
-    getUser(auth.currentUser === null ? null: auth.currentUser.uid)
-        .then((user: UserData) =>
-            addDoc(collection(db, 'article'), {
-                author_uid: user.uid,
-                author_image: user.profile_image,
-                author_username: user.username,
-                content: content,
-                edit_time: serverTimestamp(),
-                header_image: header_image,
-                published: published,
-                publish_time: published ? serverTimestamp() : null,
-                title: title,
-            }).then(
-                (doc) => {
-                setLoading(false)
-                setArticleId(doc.id)
-            },
-            (err) => {
-                setError(err.code)
-            }))
-        .catch((err) => {
-          setError("unauthenticated")
-        })
-    };
-
-  return {createArticle, error, loading, articleId }
+  return { createArticle, error, loading, articleId }
 }
 
 export const useArticleEdit = () => {
@@ -221,11 +230,11 @@ export const useArticleEdit = () => {
   const [loading, setLoading] = useState(true)
 
   const editArticle = (
-      articleID: string,
-      title: string,
-      content: string,
-      header_image: string,
-      published: boolean,
+    articleID: string,
+    title: string,
+    content: string,
+    header_image: string,
+    published: boolean,
   ) => {
     updateDoc(doc(db, 'article', articleID), {
       content: content,
@@ -236,15 +245,15 @@ export const useArticleEdit = () => {
       title: title,
     }).then(
       (doc) => {
-      setLoading(false)
+        setLoading(false)
       },
       (err) => {
-       setError(err.code)
-    }
+        setError(err.code)
+      },
     )
-  };
+  }
 
-  return {editArticle, error, loading }
+  return { editArticle, error, loading }
 }
 
 export const useArticlePost = () => {
@@ -264,9 +273,9 @@ export const useArticlePost = () => {
         setError(err.code)
       },
     )
-  };
+  }
 
-  return {postArticle, error, loading }
+  return { postArticle, error, loading }
 }
 
 export const useArticleDelete = (articleID: string) => {
@@ -282,7 +291,7 @@ export const useArticleDelete = (articleID: string) => {
         setError(err.code)
       },
     )
-  };
+  }
 
-  return {deleteArticle, error, loading }
+  return { deleteArticle, error, loading }
 }
--- src/pages/updatearticle/UpdateArticle.tsx
+++ src/pages/updatearticle/UpdateArticle.tsx
@@ -1,206 +1,40 @@
-import { Box, Button, FormLabel, Stack } from '@mui/material'
-import { Container } from '@mui/system'
-import React, { useState, FormEvent } from 'react'
-import { LabeledTextField } from '../../components/LabeledTextField'
-import { useArticleCreate } from '../../hooks/firebase/useArticle'
+import { AppWrapper } from '../../components/AppWrapper'
+import { ArticleForm, ArticleFormPurpose } from '../../components/ArticleForm'
+import { useArticleEdit, useArticleRead } from '../../hooks/firebase/useArticle'
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-const pictureUrls = [
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-]
-
-export const UpdateArticle = () => {
-  const [pictureIndexStart, setPictureIndexStart] = useState(0)
-  const [selectedPictureIndex, setSelectedPictureIndex] = useState(0)
-
-  const [isTitleError, setIsTitleError] = useState(false)
-  const [title, setTitle] = useState('')
-  const [titleHelperText, setTitleHelperText] = useState('')
-
-  const [isBodyError, setIsBodyError] = useState(false)
-  const [body, setBody] = useState('')
-  const [bodyHelperText, setBodyHelperText] = useState('')
-
-  const [customLink, setCustomLink] = useState('')
-
-  const handleSubmit = (e: FormEvent<HTMLElement>, published: boolean) => {
-    let isInvalid = false
-    if (title.length === 0 || countWords(title) > 60) {
-      isInvalid = true
-      setIsTitleError(true)
-      if (title.length === 0) {
-        setTitleHelperText("Title can't be empty.")
-      } else {
-        setBodyHelperText('Title must be 60 words or less.')
-      }
-    } else {
-      setIsTitleError(false)
-      setTitleHelperText('')
-    }
-    if (body.length === 0 || countWords(body) > 250) {
-      isInvalid = true
-      setIsBodyError(true)
-      if (body.length === 0) {
-        setBodyHelperText("Body can't be empty.")
-      } else {
-        setBodyHelperText('Body must be 250 words or less.')
-      }
-    } else {
-      console.log(countWords(body))
-      setIsBodyError(false)
-      setBodyHelperText('')
-    }
-    if (!isInvalid) {
-      useArticleCreate(
-        title,
-        body,
-        customLink.length > 0 ? customLink : pictureUrls[selectedPictureIndex],
-        published,
-      )
-    }
-
-    e.preventDefault()
-  }
-  return (
-    <Container>
-      <form
-        style={{
-          alignSelf: 'stretch',
-          justifyItems: 'space-between',
-          flex: 1,
-        }}
-        onSubmit={(event) => {
-          handleSubmit(event, true)
-        }}
-      >
-        <Stack
-          direction='column'
-          justifyContent='flex-start'
-          alignItems='flex-start'
-          spacing={40}
-        >
-          <Button
-            variant='contained'
-            style={{ backgroundColor: 'black', alignSelf: 'flex-end' }}
-            onClick={(event) => {
-              handleSubmit(event, false)
-            }}
-          >
-            SAVE DRAFT
-          </Button>
-          <FormLabel style={{ color: 'black' }}>Pick an image</FormLabel>
-          <Stack
-            justifyContent={'space-between'}
-            alignSelf={'stretch'}
-            direction='row'
-          >
-            <Button
-              style={{ color: 'black' }}
-              onClick={() => {
-                if (pictureIndexStart - 4 < 0) {
-                  setPictureIndexStart(
-                    Math.ceil(pictureUrls.length / 4) * 4 - 4,
-                  )
-                } else {
-                  setPictureIndexStart(
-                    (pictureIndexStart - 4) % pictureUrls.length,
-                  )
-                }
-              }}
-            >
-              {'<'}
-            </Button>
-            <Stack direction='row' spacing={5}>
-              {Array(4)
-                .fill(null)
-                .map((value, index) => {
-                  return (
-                    <Button
-                      style={{
-                        backgroundColor:
-                          selectedPictureIndex === pictureIndexStart + index
-                            ? 'black'
-                            : 'transparent',
-                      }}
-                      disabled={!pictureUrls[pictureIndexStart + index]}
-                      key={index}
-                      onClick={() => {
-                        setSelectedPictureIndex(pictureIndexStart + index)
-                      }}
-                    >
-                      <Box
-                        sx={{
-                          width: 150,
-                          height: 150,
-                          backgroundImage: `url(${
-                            pictureUrls[pictureIndexStart + index]
-                          })`,
-                        }}
-                      />
-                    </Button>
-                  )
-                })}
-            </Stack>
-            <Button
-              style={{ color: 'black' }}
-              onClick={() => {
-                if (pictureIndexStart + 4 >= pictureUrls.length) {
-                  setPictureIndexStart(0)
-                } else {
-                  setPictureIndexStart(
-                    (pictureIndexStart + 4) % pictureUrls.length,
-                  )
-                }
-              }}
-            >
-              {'>'}
-            </Button>
-          </Stack>
-          <LabeledTextField
-            variant='outlined'
-            onTextChange={setCustomLink}
-            placeholder='Paste link to image'
-            label='or'
-            multiline={false}
-          />
-          <LabeledTextField
-            variant='outlined'
-            onTextChange={setTitle}
-            placeholder='60 words or less'
-            label='Title'
-            multiline={false}
-            error={isTitleError}
-            helperText={titleHelperText}
-          />
-          <LabeledTextField
-            variant='outlined'
-            onTextChange={setBody}
-            placeholder='250 words or less'
-            label='Body'
-            multiline={true}
-            rows={7}
-            error={isBodyError}
-            helperText={bodyHelperText}
-          />
-        </Stack>
-        <Button
-          type='submit'
-          variant='contained'
-          style={{ marginTop: 34, backgroundColor: 'black' }}
-        >
-          CREATE
-        </Button>
-      </form>
-    </Container>
-  )
+interface UpdateArticleProps {
+  articleIdForUpdate: string
 }
 
-function countWords(text: string): number {
-  return text.trim().split(' ').length
+export const UpdateArticle = ({ articleIdForUpdate }: UpdateArticleProps) => {
+  const {
+    editArticle,
+    error: errorArticleUpdate,
+    loading: loadingArticleUpdate,
+  } = useArticleEdit()
+  const {
+    error: errorArticleRead,
+    loading: loadingArticleRead,
+    article,
+  } = useArticleRead(articleIdForUpdate)
+  return (
+    <AppWrapper>
+      <ArticleForm
+        purpose={ArticleFormPurpose.UPDATE}
+        onSubmit={(
+          title: string,
+          body: string,
+          imagelink: string,
+          published: boolean,
+          articleId?: string,
+        ) => {
+          if (articleId !== undefined) {
+            editArticle(articleId, title, body, imagelink, published)
+          } else {
+            // TODO add error handling
+          }
+        }}
+      />
+    </AppWrapper>
+  )
 }
