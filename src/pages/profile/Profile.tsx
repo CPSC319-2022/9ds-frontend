46519e8771bd0c2c344bbe46fbeb9d8599ecde39
patrick <pgousse@student.ubc.ca>
2023-03-13 12:13:39 -0400

    add article hooks

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -3,19 +3,32 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { Article } from '../../components/Article'
 import { LabeledTextField } from '../../components/LabeledTextField'
-import { useUser } from '../../hooks/firebase/useUser'
+import {
+  useUser,
+  useUserArticles,
+  useUserDrafts,
+} from '../../hooks/firebase/useUser'
 import { CircularProgress, Box } from '@mui/material'
-import { TEST_ARTICLE } from '../../configs/testArticle'
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 import { NotificationContext } from '../../context'
 
 export const Profile: FC = () => {
   const { error, loading, queriedUser } = useUser()
+  const {
+    articles: UserArticles,
+    loading: loadingArticles,
+    error: articleError,
+  } = useUserArticles(queriedUser.uid, 4)
+  const {
+    articles: UserDrafts,
+    loading: loadingDrafts,
+    error: draftError,
+  } = useUserDrafts(4)
   const { dispatch } = useContext(NotificationContext)
 
   useEffect(() => {
-    if (error) {
+    if (error || articleError || draftError) {
       dispatch({
         notificationActionType: 'error',
         message: `Error fetching profile information: ${error}`,
@@ -26,7 +39,7 @@ export const Profile: FC = () => {
   return (
     <Stack direction='column' spacing={32} boxSizing='border-box' p='24px'>
       <Header />
-      {loading ? (
+      {loading || loadingArticles || loadingDrafts ? (
         <Box
           sx={{
             display: 'flex',
@@ -89,8 +102,8 @@ export const Profile: FC = () => {
             Posts
           </Typography>
           <Stack direction='row' spacing={16} justifyContent='flex-start'>
-            {[...Array(4).keys()].map((key) => (
-              <Article key={key} size='small' article={TEST_ARTICLE} />
+            {[...UserArticles].map((article) => (
+              <Article key={article.articleId} size='small' article={article} />
             ))}
           </Stack>
           <Typography
@@ -101,8 +114,8 @@ export const Profile: FC = () => {
             Drafts
           </Typography>
           <Stack direction='row' spacing={16} justifyContent='flex-start'>
-            {[...Array(4).keys()].map((key) => (
-              <Article key={key} article={TEST_ARTICLE} />
+            {[...UserDrafts].map((draft) => (
+              <Article key={draft.articleId} article={draft} />
             ))}
           </Stack>
         </>
