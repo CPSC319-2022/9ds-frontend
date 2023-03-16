f3d40402fefc03bcc3cd081f358a511ad2574fcb
patrick <pgousse@student.ubc.ca>
2023-03-16 13:49:27 -0400

    added notification on click

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -41,6 +41,10 @@ export const Profile: FC = () => {
 
   const handleButtonClick = () => {
     applyPromotion()
+    dispatch({
+      notificationActionType: 'success',
+      message: `Successfully applied to become contributor!`,
+    })
   }
 
   return (
