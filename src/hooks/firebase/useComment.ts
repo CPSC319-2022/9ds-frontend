f607a593d5ea03c991fa61b7849fcbf705f4a529
StuartLiv <h.stuart.livingstone@gmail.com>
2023-03-20 21:31:02 -0700

    bug fixes

--- src/hooks/firebase/useComment.ts
+++ src/hooks/firebase/useComment.ts
@@ -34,7 +34,7 @@ export const useCommentCreate = () => {
       .then((user) =>
         addDoc(collection(db, `article/${articleID}/comments`), {
           commenter_uid: user.uid,
-          profile_image: user.profile_image,
+          commenter_image: user.profile_image,
           commenter_username: user.username,
           content: content,
           post_time: serverTimestamp(),
