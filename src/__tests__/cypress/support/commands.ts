c9afb02b906a5a9432529c9178a438a2f5c7d8f4
dongandy <andydong0928@gmail.com>
2023-03-22 01:01:01 -0700

    suppress no-namespace for eslint

--- src/__tests__/cypress/support/commands.ts
+++ src/__tests__/cypress/support/commands.ts
@@ -40,6 +40,7 @@ Cypress.Commands.add('login', (email, password) => {
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
+// eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   interface Chainable {
     login(email: string, password: string): Chainable<void>
