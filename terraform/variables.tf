c422b8316f89188c283441a2061869e24ac9c2c5
Long Nguyen <longnguyen2001@gmail.com>
2023-03-31 19:03:26 -0700

    Add slack notifier files from devops repo and refactored existing tf files

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "dev_project_id" {
+  description = "ProjectID of the devops project"
+  type        = string
+  default     = "ds-blog-dev"
+}
+
+variable "qa_project_id" {
+  description = "ProjectID of the devops project"
+  type        = string
+  default     = "ds-blog-qa"
+}
+
+variable "prod_project_id" {
+  description = "ProjectID of the devops project"
+  type        = string
+  default     = "ds-blog-376905"
+}
