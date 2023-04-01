775109ed50142b1725b9c0cd83c038624f8775a2
personalbrian <59339384+personalbrian@users.noreply.github.com>
2023-03-31 22:07:21 -0700

    PWC-144 email form test + 80 coverage (#109)
    * wip
    
    * wip
    
    * quick fix plz forgive me
    
    * forgot code
    
    * WIP
    
    * 80 complete
    
    ---------
    
    Co-authored-by: brian <r7c2b@ugrad.cs.ubc.ca>
    Co-authored-by: dongandy <andydong0928@gmail.com>

--- src/__tests__/components/EmailVerificationForm.spec.tsx
+++ src/__tests__/components/EmailVerificationForm.spec.tsx
@@ -1,81 +1,144 @@
-import { render, screen } from '@testing-library/react'
-import { EmailVerificationForm } from '../../components/EmailVerificationForm'
-import { MemoryRouter as Router } from 'react-router-dom'
-import userEvent from '@testing-library/user-event'
+import {fireEvent, render, screen} from '@testing-library/react'
+import {EmailVerificationForm} from '../../components/EmailVerificationForm'
+import {MemoryRouter as Router} from 'react-router-dom'
+import React from 'react'
+import {useForgotPasswordEmail} from '../../hooks/firebase/useAuth'
+
+jest.mock('../../hooks/firebase/useAuth', () => ({
+  ...jest.requireActual('../../hooks/firebase/useAuth'),
+  useForgotPasswordEmail: jest.fn(),
+}))
+
+const mockNavigate = jest.fn()
+jest.mock('react-router-dom', () => ({
+    ...(jest.requireActual('react-router-dom') as any),
+    useNavigate: () => mockNavigate,
+  }))
+
+const mockSendEmail = jest.fn()
 
 describe('EmailVerificationForm', () => {
-  beforeAll(() => {
-    render(
-      <Router>
-        <EmailVerificationForm />
-      </Router>,
-    )
-    jest.setTimeout(15000)
-  })
+  beforeEach(() => {
+    jest.resetModules();
+    useForgotPasswordEmail.mockReturnValue({
+      sendEmail: jest.fn(),
+      error: {code: ''},
+      loading: false
+    })
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   test('style', () => {
-    const message = screen.getByText('SEND RESET PASSWORD LINK')
-    expect(message).toBeInTheDocument()
-    const button = screen.getByRole('button')
-    expect(button).toBeInTheDocument()
+    render(
+      <Router>
+        <EmailVerificationForm/>
+      </Router>
+    );
+    const message = screen.getByText('SEND RESET PASSWORD LINK');
+    expect(message).toBeInTheDocument();
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
   })
 
   test('renders email input field', () => {
     render(
       <Router>
-        <EmailVerificationForm />
-      </Router>,
-    )
+        <EmailVerificationForm/>
+      </Router>
+    );
     const emailInput = screen.getByLabelText('Email')
-    expect(emailInput).toBeInTheDocument()
+    expect(emailInput).toBeInTheDocument();
   })
 
   test('renders send button', () => {
     render(
       <Router>
-        <EmailVerificationForm />
-      </Router>,
-    )
+        <EmailVerificationForm/>
+      </Router>
+    );
     const sendButton = screen.getByRole('button', {
       name: /send reset password link/i,
-    })
-    expect(sendButton).toBeInTheDocument()
-  })
+    });
+    expect(sendButton).toBeInTheDocument();
+  });
 
   test('shows error message when email is invalid', () => {
+    useForgotPasswordEmail.mockReturnValue({
+          sendEmail: mockSendEmail,
+          error: {code: 'auth/invalid-email'},
+          loading: true
+    })
     render(
       <Router>
-        <EmailVerificationForm />
+        <EmailVerificationForm/>
       </Router>,
     )
     const emailInput = screen.getByLabelText('Email')
+    fireEvent.change(emailInput, {target: {value: 'invalid-email'}});
     const sendButton = screen.getByRole('button', {
       name: /send reset password link/i,
+    });
+    fireEvent.click(sendButton);
+    expect(screen.getByText(/invalid email/i)).toBeInTheDocument();
+    expect(mockSendEmail).toBeCalled();
+  });
+
+  test('shows error message when user not found', () => {
+    useForgotPasswordEmail.mockReturnValue({
+          sendEmail: mockSendEmail,
+          error: {code: 'auth/user-not-found'},
+          loading: true
     })
-
-    userEvent.type(emailInput, 'invalid-email')
-    userEvent.click(sendButton)
-
-    //TODO: replace with actual error message
-    // const errorMessage = screen.getByText('Invalid email')
-    // expect(errorMessage).toBeInTheDocument()
-  })
-
-  test('shows error message when user is not found', () => {
     render(
       <Router>
-        <EmailVerificationForm />
+        <EmailVerificationForm/>
       </Router>,
     )
     const emailInput = screen.getByLabelText('Email')
+    fireEvent.change(emailInput, {target: {value: 'dummy@mail.com'}});
     const sendButton = screen.getByRole('button', {
       name: /send reset password link/i,
+    });
+    fireEvent.click(sendButton);
+    expect(screen.getByText(/user was not found/i)).toBeInTheDocument();
+    expect(mockSendEmail).toBeCalled();
+  });
+
+  test('shows error message when unknown error', () => {
+    useForgotPasswordEmail.mockReturnValue({
+          sendEmail: mockSendEmail,
+          error: {code: 'unknown'},
+          loading: true
     })
+    render(
+      <Router>
+        <EmailVerificationForm/>
+      </Router>,
+    )
+    const emailInput = screen.getByLabelText('Email')
+    fireEvent.change(emailInput, {target: {value: 'dummy@mail.com'}});
+    const sendButton = screen.getByRole('button', {
+      name: /send reset password link/i,
+    });
+    fireEvent.click(sendButton);
+    expect(screen.getByText(/unable to send email link/i)).toBeInTheDocument();
+    expect(mockSendEmail).toBeCalled();
+  });
 
-    userEvent.type(emailInput, 'not-found@example.com')
-    userEvent.click(sendButton)
+  test('navigate to login page when success', () => {
+    useForgotPasswordEmail.mockReturnValue({
+          sendEmail: mockSendEmail,
+          error: {code: 'unknown'},
+          loading: false
+    })
+    render(
+      <Router>
+        <EmailVerificationForm/>
+      </Router>,
+    )
+    expect(mockNavigate).toBeCalledWith('/get-started');
+  });
 
-    // const errorMessage = screen.getByText('User was not found')
-    // expect(errorMessage).toBeInTheDocument()
-  })
-})
+});
