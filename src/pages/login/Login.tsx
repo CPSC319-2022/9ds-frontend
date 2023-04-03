a63dbd5949491d59a5ce2b61c451da61c2fb4093
Jay <mantine08@gmail.com>
2023-04-03 13:48:06 -0700

    Changed redirection logic to check for queried user instead of auth

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,12 +1,20 @@
-import React, { FC, useEffect } from 'react'
+import { FC, useContext, useEffect } from 'react'
 import { GetStartedTitle } from './GetStartedTitle'
 import { LoginAndSignUpForm } from './LoginAndSignUpForm'
 import { AppWrapper } from '../../components/AppWrapper'
 import { useNavigate } from 'react-router-dom'
-import { useUser } from '../../hooks/firebase/useUser'
-import { useAuth } from 'hooks/firebase/useAuth'
+import { UserContext } from 'context/UserContext'
 
 export const Login: FC = () => {
+  const { state } = useContext(UserContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (state.queriedUser.uid) {
+      navigate('/')
+    }
+  }, [state])
+
   return (
     <AppWrapper>
       <GetStartedTitle />
