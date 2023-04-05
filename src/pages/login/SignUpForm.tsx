c70d19a1fd97e7a6da1bae5787d30bb6f9caf7f8
Jay <mantine08@gmail.com>
2023-04-04 20:22:37 -0700

    no message

--- src/pages/login/SignUpForm.tsx
+++ src/pages/login/SignUpForm.tsx
@@ -20,16 +20,13 @@ import {
 } from '../../hooks/firebase/useAuth'
 import { FileUploader } from '../../components/FileUploader/FileUploader'
 import { storage } from '../../firebaseApp'
-import {getDownloadURL, ref, uploadBytes} from "@firebase/storage"
+import { getDownloadURL, ref, uploadBytes } from '@firebase/storage'
 import { NotificationContext } from '../../context/NotificationContext'
 import { useNavigate } from 'react-router-dom'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const uuid = require('uuid')
 
-
 export const SignUpForm = () => {
-
-
   const [email, setEmail] = useState('')
   const [isEmailError, setIsEmailError] = useState(false)
   const [emailHelperText, setEmailHelperText] = useState('')
@@ -57,20 +54,13 @@ export const SignUpForm = () => {
 
   const emailAccountCreate = useCreateUserEmailPassword()
   const signInWithGoogle = useSignInWithGoogle()
-  
+
   const navigate = useNavigate()
 
   useEffect(() => {
     if (emailAccountCreate.user) {
-        navigate('/')
-    }
-  }, [emailAccountCreate.user])
-
- const navigate = useNavigate()
-  useEffect(() => {
-    if (emailAccountCreate.user) {
-        navigate('/')
-        window.location.reload()
+      navigate('/')
+      window.location.reload()
     }
   }, [emailAccountCreate.user])
 
@@ -78,12 +68,12 @@ export const SignUpForm = () => {
   // https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#createuserwithemailandpassword
   useEffect(() => {
     const error = emailAccountCreate.error?.toString() ?? ''
-    if (error.includes("storage")) {
-        dispatch({
-            notificationActionType: 'error',
-            message: 'Error uploading image',
-        })
-        return
+    if (error.includes('storage')) {
+      dispatch({
+        notificationActionType: 'error',
+        message: 'Error uploading image',
+      })
+      return
     }
     switch (error) {
       case 'auth/email-already-in-use':
@@ -123,10 +113,7 @@ export const SignUpForm = () => {
     }
   }, [emailAccountCreate.error])
 
-
-
   const handleSignUp = (e: FormEvent<HTMLElement>) => {
-    
     let isInvalid = false
     if (!email.length) {
       isInvalid = true
@@ -155,12 +142,14 @@ export const SignUpForm = () => {
       setPasswordHelperText('')
     }
     if (!isInvalid) {
-        emailAccountCreate.createWithEmailAndPasswordWrapper(
-            email,
-            password,
-            name,
-            file != null ? file : 'https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg',
-        )
+      emailAccountCreate.createWithEmailAndPasswordWrapper(
+        email,
+        password,
+        name,
+        file != null
+          ? file
+          : 'https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg',
+      )
     }
     e.preventDefault()
   }
@@ -256,12 +245,12 @@ export const SignUpForm = () => {
           )}
         </FormControl>
         <Typography
-            variant='body1.medium'
-            sx={{ fontSize: '14px', weight: 600 }}
-          >
-            Optional: Select a Profile Picture
-          </Typography>
-        <FileUploader setFile={setFile} file={file}/>
+          variant='body1.medium'
+          sx={{ fontSize: '14px', weight: 600 }}
+        >
+          Optional: Select a Profile Picture
+        </Typography>
+        <FileUploader setFile={setFile} file={file} />
         <Button
           variant='contained'
           sx={{
@@ -280,4 +269,3 @@ export const SignUpForm = () => {
 const IMAGE_LINK_REGEX = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/
 
 export const isProfImgLinkValid = (link: string) => IMAGE_LINK_REGEX.test(link)
-
