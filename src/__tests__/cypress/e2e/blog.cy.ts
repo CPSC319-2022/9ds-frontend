2ad1226cda94614078c8909531b478587e951119
dongandy <andydong0928@gmail.com>
2023-04-04 21:26:31 -0700

    fixed a small warning

--- src/__tests__/cypress/e2e/blog.cy.ts
+++ src/__tests__/cypress/e2e/blog.cy.ts
@@ -3,27 +3,15 @@ describe('QA website blog', () => {
     cy.visit('/')
   })
 
-  it('should bring unauthenticated user to sign in page when clicking BLOG in the header', () => {
-    cy.get('a').contains('BLOG').click()
-    cy.url().should('include', '/login')
-  })
-
-  it('should bring authenticated user to create blog page when clicking BLOG in the header', () => {
-    cy.get('a').contains('Login').click()
-    cy.login('reader@test.com', 'reader123')
-    cy.get('a').contains('BLOG').click()
+  it('should bring authenticated user to create blog page when clicking CREATE BLOG POST in the header', () => {
+    cy.get('button').contains('LOGIN/SIGN UP').click()
+    cy.login('contributor@test.com', 'contributor123')
+    cy.get('a').contains('CREATE BLOG POST').click()
     cy.url().should('include', '/create')
   })
 
-  it('should bring unauthenticated user to sign in page when clicking Blog in the footer', () => {
-    cy.get('a').contains('Blog').click()
-    cy.url().should('include', '/login')
-  })
-
-  it('should bring authenticated user to create blog page when clicking Blog in the footer', () => {
-    cy.get('a').contains('Login').click()
-    cy.login('reader@test.com', 'reader123')
-    cy.get('a').contains('Blog').click()
+  it('should bring authenticated user to create blog page when clicking Create blog in the footer', () => {
+    cy.get('button').contains('Create blog').click()
     cy.url().should('include', '/create')
   })
 })
